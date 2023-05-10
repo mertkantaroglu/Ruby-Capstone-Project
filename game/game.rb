479020844b0b0122958c4f4281347e8bb3e3490c
Lugard Agu <agulugard@gmail.com>
2023-05-10 11:57:10 +0100

    Resolve issues

--- game/game.rb
+++ game/game.rb
@@ -6,10 +6,10 @@ class Game < Item
   def initialize(multiplayer, last_played_at, publish_date)
     super(publish_date)
     @multiplayer = multiplayer
-    @last_played_at = Date.iso8601(last_played_at)
+    @last_played_at = last_played_at
   end
 
   def can_be_archived?
-    super || @last_played_at < 2.years.ago
+    super && @last_played_at < 2.years.ago
   end
 end
