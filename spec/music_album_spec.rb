5ebd8af0e2838e445f7b2bd9fc7e0769c4c992c4
unknown <sohailshahidi1435@gmail.com>
2023-05-10 19:18:35 +0430

    Add required changes

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,21 +1,19 @@
 require './music/music_album'
 
 describe 'MusicAlbum' do
-  before(:each) do
-    @album = MusicAlbum.new(on_spotify: true, can_be_archived: true)
-  end
+  let(:album) { MusicAlbum.new(on_spotify: true, can_be_archived: true) }
 
   context 'testing MusicAlbum class' do
     it 'create the instance of MusicAlbum class' do
-      expect(@album).to be_an_instance_of(MusicAlbum)
+      expect(album).to be_an_instance_of(MusicAlbum)
     end
 
     it 'should return correct spotify status' do
-      expect(@album.on_spotify).to eql(true)
+      expect(album.on_spotify).to eql(true)
     end
 
-    it 'should return the correct date' do
-      expect(@album.can_be_archived).to eql(true)
+    it 'should return the correct value for the can_be_archived attribute' do
+      expect(album.can_be_archived).to eql(true)
     end
   end
 end
