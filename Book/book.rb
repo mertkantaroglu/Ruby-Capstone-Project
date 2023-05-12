e2e3d78da10a62b4aeedde59c08178a4fc98f265
Nezerwa <eligrand2000@gmail.com>
2023-05-12 16:17:39 +0200

    Add item class inheritance and book attributes initialization.

--- Book/book.rb
+++ Book/book.rb
@@ -1,6 +1,7 @@
 require_relative '../item'
 class Book < Item
   attr_accessor :id, :name, :publish_date, :publisher, :cover_state
+
   def initialize(name, publish_date, publisher, cover_state)
     super(nil, nil, nil, nil, publish_date)
     @id = id || Random.rand(1..1000)
@@ -8,8 +9,9 @@ class Book < Item
     @publisher = publisher
     @cover_state = cover_state
   end
+
   def can_be_archived?
     super || @cover_state == 'bad'
   end
   private :can_be_archived?
-end 
+end
