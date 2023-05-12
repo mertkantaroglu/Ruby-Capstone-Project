331c6d3c1b026652cce52f897ff244d93b109bbb
Nezerwa <eligrand2000@gmail.com>
2023-05-12 16:06:11 +0200

    Refactor data saving and loading with JSON using modules.

--- modules/book_storage.rb
+++ modules/book_storage.rb
@@ -1,4 +1,5 @@
 require 'json'
+
 module SaveBookData
   def save_book(books)
     save_data(books) do |book|
@@ -10,6 +11,7 @@ module SaveBookData
       }
     end
   end
+
   def save_label(labels)
     save_data(labels) do |label|
       {
@@ -18,18 +20,23 @@ module SaveBookData
       }
     end
   end
+
   private
+
   def save_data(data)
     save_data = data.map { |item| yield(item) }
     write_to_file(save_data)
   end
+
   def write_to_file(data)
     File.write(file_path, JSON.generate(data))
   end
+
   def file_path
-    ‘./data/books.json’
+    './data/books.json'
   end
 end
+
 module LoadBookData
   def load_books
     load_data('./data/books.json') do |book_hash|
@@ -41,12 +48,15 @@ module LoadBookData
       )
     end
   end
+
   def load_labels
     load_data('./data/labels.json') do |label_hash|
       Label.new(label_hash['title'], label_hash['color'])
     end
   end
+
   private
+
   def load_data(file_path)
     if File.exist?(file_path)
       json_data = File.read(file_path)
