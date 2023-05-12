0528dcfe402762f194985cc91faa02aeff91fd18
Nezerwa <eligrand2000@gmail.com>
2023-05-12 14:44:00 +0200

    Add Label class with add_label method.

--- Book/label.rb
+++ Book/label.rb
@@ -1,13 +1,11 @@
 class Label
   attr_reader :id, :title, :color, :items
-
-  def initialize(id, title, color)
+  def initialize(title, color)
     @id = id || Random.rand(1..1000)
     @title = title
     @color = color
     @items = []
   end
-
   def add_label(item)
     @items << item unless @items.include?(item)
     item.label = self unless item.label == self
